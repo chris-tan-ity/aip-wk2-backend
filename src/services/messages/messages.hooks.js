662814960ad5911e372bc987c0e6290273f6063d
Chris Tan <christanity@gmail.com>
2017-08-08 02:33:02 +1000

    initial commit

---
+++ src/services/messages/messages.hooks.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { authenticate } = require('feathers-authentication').hooks;
+const { populate } = require('feathers-hooks-common');
+const processMessage = require('../../hooks/process-message');
+
+
+module.exports = {
+  before: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
